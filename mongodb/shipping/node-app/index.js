2118aa4c45c3b542180afb0a6dad74b0a305f183
Ovidio R. Calvet <calveto@gmail.com>
2018-08-23 22:21:12 -0400

    Add other services

--- mongodb/shipping/node-app/index.js
+++ mongodb/shipping/node-app/index.js
@@ -1,12 +1,56 @@
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const app = express();
 const port = process.env.APP_PORT || 8080;
+const mongoUrl = process.env.DB_URL || 'mongodb://localhost:27017';
+const connectOptions = { 
+    useNewUrlParser: true, 
+};
+const dbname = 'shippings';
+
+app.get('/ping', (req, res) => res.end('pong'));
 
 app.get('/', (req, res) => {
-    res.json([{
-        _id: '123444',
-        name: 'checking 1'
-    }]);
+    MongoClient.connect(mongoUrl, connectOptions,  (err, client) => {
+        if (err) {
+            res.statusCode = 400;
+            res.json({...err});
+            return;
+        }
+        db = client.db(dbname);
+        const collection = db.collection('documents');
+        collection.find({}).toArray((err, docs) => {
+            if (err) {
+                res.statusCode = 400;
+                res.json({...err});
+                return;
+            }
+            res.json(docs);
+        });
+    });
+});
+
+app.post('/seed', (req, res) => {
+    MongoClient.connect(mongoUrl, connectOptions, (err, client) => {
+        if (err) {
+            res.statusCode = 400;
+            res.json({...err});
+            return;
+        }
+        db = client.db(dbname);
+        const collection = db.collection('documents');
+        collection.insertMany([
+            { address: '1234 abc st', date: '2018/04/11' },
+            { address: '567 fdg st', date: '2017/01/02'  },
+        ], (err, result) => {
+            if (err) {
+                res.statusCode = 400;
+                res.json({...err});
+                return;
+            }
+            res.json(result.insertedIds);
+        });
+    });
 });
 
 app.listen(port, () => {
--- mongodb/users/node-app/index.js
+++ mongodb/users/node-app/index.js
@@ -1,12 +1,57 @@
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const app = express();
 const port = process.env.APP_PORT || 8080;
+const mongoUrl = process.env.DB_URL || 'mongodb://localhost:27017';
+const connectOptions = { 
+    useNewUrlParser: true, 
+};
+const dbname = 'users';
+
+app.get('/ping', (req, res) => res.end('pong'));
 
 app.get('/', (req, res) => {
-    res.json([{
-        _id: '123444',
-        name: 'checking 1'
-    }]);
+    MongoClient.connect(mongoUrl, connectOptions,  (err, client) => {
+        if (err) {
+            res.statusCode = 400;
+            res.json({...err});
+            return;
+        }
+        db = client.db(dbname);
+        const collection = db.collection('documents');
+        collection.find({}).toArray((err, docs) => {
+            if (err) {
+                res.statusCode = 400;
+                res.json({...err});
+                return;
+            }
+            res.json(docs);
+        });
+    });
+});
+
+app.post('/seed', (req, res) => {
+    MongoClient.connect(mongoUrl, connectOptions, (err, client) => {
+        if (err) {
+            res.statusCode = 400;
+            res.json({...err});
+            return;
+        }
+        db = client.db(dbname);
+        const collection = db.collection('documents');
+        collection.insertMany([
+            { name: 'Jack', age: 23 },
+            { name: 'Peter', age: 43 },
+            { name: 'Jane', age: 16 },
+        ], (err, result) => {
+            if (err) {
+                res.statusCode = 400;
+                res.json({...err});
+                return;
+            }
+            res.json(result.insertedIds);
+        });
+    });
 });
 
 app.listen(port, () => {
